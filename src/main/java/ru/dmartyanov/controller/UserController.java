53098065414223c241b3957b195d1743c9e9ced2
Jzzza <jzzza1@gmail.com>
2020-10-13 20:50:47 +0300

    Add rest controller for user entity

---
+++ src/main/java/ru/dmartyanov/controller/UserController.java
@@ -0,0 +1,24 @@
+package ru.dmartyanov.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.dmartyanov.domain.model.User;
+import ru.dmartyanov.domain.model.UserRepository;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user/all")
+    public List<User> allUse() {
+        return userRepository.findAll();
+    }
+}
